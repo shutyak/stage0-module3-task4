5d6ae931004ac465bfecd21726104f57ce5a4219
vlshutyak <vl.shutyak@gmail.com>
2023-04-12 18:35:42 +0300

    s0-m2-t3

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a=1,b=1;
+        int formula = (9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
+
+
     }
 }
