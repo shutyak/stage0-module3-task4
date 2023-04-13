6faf372746b42efb44e1097bc333b2b2466d65a6
vlshutyak <vl.shutyak@gmail.com>
2023-04-13 22:18:57 +0300

    s0-m3-t4 FormulaCreator fix

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,9 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a=1,b=1;
-        int formula = (9*a*a-5*b+2+a-7)*((a+b-4*a*b)/2);
-
-
+        int a = 1, b = 1;
+        int formula = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
     }
 }
