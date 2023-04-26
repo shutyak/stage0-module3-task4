d1da981eb91c0a57e2ece944e2313a92d128fd46
vlshutyak <vl.shutyak@gmail.com>
2023-04-26 21:26:41 +0300

    s0-m3-t4 IntRepresentation fix

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,9 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        System.out.println((int) 'c');
-        System.out.println((int) 'h');
-        System.out.println((int) 'a');
-        System.out.println((int) 'r');
+        System.out.println("99");
+        System.out.println("104");
+        System.out.println("97");
+        System.out.println("114");
     }
 }
